57e6e93daf15a415d9317681180636da9a894494
jessesbyers <jesse.s.byers@gmail.com>
2020-02-03 12:33:46 -0500

    seed users added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(:username => "TestUser1", :email => "test1@email.com", :password => "password1")
+User.create(:username => "TestUser2", :email => "test2@email.com", :password => "password2")
+User.create(:username => "TestUser3", :email => "test3@email.com", :password => "password3")
+
