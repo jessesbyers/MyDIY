e95df78cf3b87ae14cb03bd24b5ef08ff678c574
jessesbyers <jesse.s.byers@gmail.com>
2020-02-03 10:16:25 -0500

    new user form and controller actions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root 'application#index'
+  # resources :users
+  root to: "users#index"
+
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
 end
