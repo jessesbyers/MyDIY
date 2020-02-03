4abc3612e083845543724764ca8f99e1c07417f3
jessesbyers <jesse.s.byers@gmail.com>
2020-02-03 10:26:38 -0500

    user validations and new user error messages added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, length: { :minimum => 6}
 end
