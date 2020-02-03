51ea47f2aa2a7fb306cab76bafaab7347f0f403d
jessesbyers <jesse.s.byers@gmail.com>
2020-02-03 10:37:51 -0500

    sessions controller and routes added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.find_by(name: params[:user][:username])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to root_path
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
 
   get '/users/new' => 'users#new'
   post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  get '/logout' => 'sessions#destroy'
+  post '/logout' => 'sessions#destroy'
 end
