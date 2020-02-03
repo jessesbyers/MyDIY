ac33d811a4e4373df9b616aa57b321681616e849
jessesbyers <jesse.s.byers@gmail.com>
2020-02-03 12:42:14 -0500

    seed passwords deleted

--- db/seeds.rb
+++
@@ -1,4 +0,0 @@
-User.create(:username => "TestUser1", :email => "test1@email.com", :password => "password1")
-User.create(:username => "TestUser2", :email => "test2@email.com", :password => "password2")
-User.create(:username => "TestUser3", :email => "test3@email.com", :password => "password3")
-
