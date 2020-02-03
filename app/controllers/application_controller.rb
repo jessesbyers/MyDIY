70cbbd6b11181e9f39b60b15a2fab5da239f28cd
jessesbyers <jesse.s.byers@gmail.com>
2020-02-03 11:14:53 -0500

    signup, login, logout basic flow working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::Base
+
+    private
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+    end
+
+
+    def logged_in?(session)
+        !!session[:user_id]
+    end
+    
+    def current_user(session)
+        User.find(session[:user_id])
+    end
+  
+    def redirect_already_logged_in
+        if logged_in?(session)
+          redirect_to root_path
+        end
+    end
+  
+    def redirect_not_logged_in
+        if !logged_in?(session)
+          redirect_to login_path
+        end
+    end
+  
+    #   def redirect_not_current_user
+    #     if !logged_in?(session) || !@sighting.user_id == current_user(session).id
+    #       redirect '/'
+    #     end
+    #   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,13 @@
 class SessionsController < ApplicationController
+    # before_action :require_login
+    # skip_before_action :require_login, only: [:index]
+
     def new
     end
     
     def create
-        @user = User.find_by(:username => params[:user][:username])
+        @user = User.find_by(username: params[:user][:username])
+        return head(:forbidden) unless @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to root_path
     end
