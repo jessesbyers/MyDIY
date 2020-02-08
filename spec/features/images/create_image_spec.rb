a3095b72e824922422ba8ccd50b4213cd2fe7dcd
jessesbyers <jesse.s.byers@gmail.com>
2020-02-07 20:12:11 -0500

    added simple feature tests

--- spec/features/images/create_image_spec.rb
+++ spec/features/images/create_image_spec.rb
@@ -24,15 +24,7 @@ include LoginHelper
 
 feature 'Sign Up' do
     scenario 'able to sign up for an account and log in' do
-        # visit '/'
-        # click_link("Sign Up")
         user_signup
-        # fill_in 'user_email', with: 'test1'
-        # fill_in 'user_username', with: 'test2'
-        # fill_in 'user_password', with: 'test300'
-        # fill_in 'user_password_confirmation', with: 'test300'
-        # click_button('Submit')
-
         expect(page).to have_content("Home: test2's Projects")
         expect(page).to have_content('Goals')
     end
@@ -42,38 +34,45 @@ end
 feature 'Add a Project' do
     scenario 'able to create a new project' do
         user_signup
-        # visit '/projects/new'
-        # click_link("Create a New Project")
+        click_link "Create A New Project"
         fill_in 'project_address', with: 'test1'
         fill_in 'project_overview', with: 'test2'
         click_button('Create Project')
+        expect(page).to have_content('test1')
+        expect(page).to have_content('test2')
     end
 end
 
 
+feature 'Add a New Goal' do
+    scenario 'able to add a new goal' do
+        user_signup
+        click_link "Create A New Project"
+        fill_in 'project_address', with: 'test1'
+        fill_in 'project_overview', with: 'test2'
+        click_button('Create Project')
+        click_link("Add a New Goal")
+        select('test1', :from => 'goal_project_id')
+        fill_in 'goal_title', with: 'test title'
+        fill_in 'goal_description', with: 'test description'
+        fill_in 'goal_budget', with: '5000'
+        select('Not Started', :from => 'goal_status')
 
+        click_button('Create Goal')
 
-
-feature 'Add an Image' do
-    scenario 'able to create an image' do
-        #1. go to goal_path
-        # visit(goal_path(:id))
-        visit 'goals/8'
-
-        #2. click on "Add an Image for this Goal"
-
-        click_link("Add an Image for this Goal")
-        # click_link "Add an Image for this Goal"
-        #3. fill in url and caption and select genre
-        fill_in 'image_url', with: 'https://i.imgur.com/n4iyLTd.png'
-        fill_in 'image_caption', with: 'test caption'
-        select('Before Photo', :from => 'image_genre')
-
-        #4. click on "Create Image" button
-        click_button('Create Image')
-
-        expect(page).to have_content('test caption')
-        expect(page).to have_content('Before Photo')
-        expect(page).to have_content('https://i.imgur.com/n4iyLTd.png')
+        expect(page).to have_content('test1')
+        expect(page).to have_content('test title')
+        expect(page).to have_content('test description')
+        expect(page).to have_content('Estimated Budget: $5000')
+        expect(page).to have_content('Status: Not Started')
     end
 end
+
+
+        # fill_in 'image_url', with: 'https://i.imgur.com/n4iyLTd.png'
+        # fill_in 'image_caption', with: 'test caption'
+        # select('Before Photo', :from => 'image_genre')
+
+        # expect(page).to have_content('test caption')
+        # expect(page).to have_content('Before Photo')
+        # expect(page).to have_content('https://i.imgur.com/n4iyLTd.png')
