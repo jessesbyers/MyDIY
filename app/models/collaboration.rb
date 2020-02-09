9dac289fafa38bdba31e4af2ce624a0ef2a0ec12
jessesbyers <jesse.s.byers@gmail.com>
2020-02-09 16:49:34 -0500

    tried refactoring scope method

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -5,6 +5,7 @@ class Collaboration < ApplicationRecord
     validates :user_id, uniqueness: { scope: :project_id, message: ": A collaboration already exists with this user on this project." }
     validates :project_id, uniqueness: { scope: :user_id, message: ": A collaboration already exists with this user on this project." }
 
+        # scope :project_user_is_primary_owner(user), -> {where(user_id: user.id, role: "Primary Project Owner").map { |c| c.project }}
     def self.project_user_is_primary_owner(user)
         where(user_id: user.id, role: "Primary Project Owner").map { |c| c.project }
     end
@@ -13,19 +14,19 @@ class Collaboration < ApplicationRecord
         where(user_id: user.id, role: "Project Owner").map { |c| c.project }
     end
 
-    def self.project_user_is_primary_or_owner(user)
-        project_user_is_primary_owner(user) + project_user_is_owner(user)
-    end
-
-    def self.project_user_is_primary_or_owner_or_update(user)
-        project_user_is_primary_owner(user) + project_user_is_owner(user) + project_user_is_update(user)
-    end
-
     def self.project_user_is_update(user)
         where(user_id: user.id, role: "Update and View").map { |c| c.project }
     end
 
     def self.project_user_is_viewonly(user)
         where(user_id: user.id, role: "View Only").map { |c| c.project }
+    end
+
+    def self.project_user_is_primary_or_owner(user)
+        project_user_is_primary_owner(user) + project_user_is_owner(user)
+    end
+
+    def self.project_user_is_primary_or_owner_or_update(user)
+        project_user_is_primary_owner(user) + project_user_is_owner(user) + project_user_is_update(user)
     end
 end
