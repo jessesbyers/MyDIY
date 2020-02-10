37e78bf4a04ddf2e1e715655b38482b0998c85d9
jessesbyers <jesse.s.byers@gmail.com>
2020-02-10 08:59:06 -0500

    image validations complete

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,9 @@ class Image < ApplicationRecord
     belongs_to :goal
     belongs_to :user
 
+    validates :url, format: { with: %r{.(gif|jpg|png)\Z}i, message: "must be a URL for GIF, JPG or PNG image." }
     GENRE_OPTIONS = ["Inspiration", "Design Plans", "Before Photo", "After Photo", "Work in Progress"]
     validates :genre, presence: true, inclusion: GENRE_OPTIONS
-    # FORMAT_OPTIONS = ["Inspiration", "Design Plans", "Before Photo", "After Photo", "Work in Progress"]
-    # validates :genre, presence: true, inclusion: GENRE_OPTIONS
+    validates :caption, presence: true
+
 end
