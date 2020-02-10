7b4108079cd212c7c08f971e397045ccfce8b7cd
jessesbyers <jesse.s.byers@gmail.com>
2020-02-10 09:30:20 -0500

    goal validations and errors complete

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -7,6 +7,7 @@ class Goal < ApplicationRecord
 
     validates :title, presence: true
     validates :description, presence: true
+    validates :budget, presence: true, numericality: true
     STATUS_OPTIONS = ["Not Started", "Incomplete", "Completed"]
     validates :status, presence: true, inclusion: STATUS_OPTIONS
 
