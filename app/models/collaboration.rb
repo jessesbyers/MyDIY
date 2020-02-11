4e99e7dd54069750f9d62a9ae99709c32470cb34
jessesbyers <jesse.s.byers@gmail.com>
2020-02-11 10:41:30 -0500

    grid styling on project show page

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -7,31 +7,37 @@ class Collaboration < ApplicationRecord
     ROLE_OPTIONS = ["Primary Project Owner", "Project Owner", "Update and View", "View Only"]
     validates :role, presence: true, inclusion: ROLE_OPTIONS
 
-
+# returns array of projects in which the (current) user is Primary Project Owner
     def self.project_user_is_primary_owner(user)
         where(user_id: user.id, role: "Primary Project Owner").map { |c| c.project }.compact
     end
 
+# returns array of projects in which the (current) user is Project Owner
     def self.project_user_is_owner(user)
         where(user_id: user.id, role: "Project Owner").map { |c| c.project }.compact
     end
 
+# returns array of projects in which the (current) user is Update and View
     def self.project_user_is_update(user)
         where(user_id: user.id, role: "Update and View").map { |c| c.project }.compact
     end
 
+# returns array of projects in which the (current) user is View Only
     def self.project_user_is_viewonly(user)
         where(user_id: user.id, role: "View Only").map { |c| c.project }.compact
     end
 
+# returns array of projects in which the (current) user is Primary OR Project Owner
     def self.project_user_is_primary_or_owner(user)
         project_user_is_primary_owner(user) + project_user_is_owner(user).compact
     end
 
+# returns array of projects in which the (current) user is Primary OR Project Owner OR Update
     def self.project_user_is_primary_or_owner_or_update(user)
         project_user_is_primary_owner(user) + project_user_is_owner(user) + project_user_is_update(user).compact
     end
 
+# returns array of projects in which the (current) user is a Collaborator in any role
     def self.project_user_is_collaborator(user)
         project_user_is_primary_or_owner_or_update(user) + project_user_is_viewonly(user)
     end
