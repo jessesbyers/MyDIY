5eacd226d343faff7504fbb3bcd6f6a4bb32a925
jessesbyers <jesse.s.byers@gmail.com>
2020-02-11 13:01:07 -0500

    collaboration model methods converted to project model methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,15 +12,45 @@ class Project < ApplicationRecord
     validates :address, presence: true, uniqueness: true
     validates :overview, presence: true
 
-    # call on a user.projects, return the group of projects
+    # call on a user.projects, 
+    # returns the array of projects in which user is any role except view_only
     def self.primary_or_owner_or_update
         joins(:collaborations).where.not('collaborations.role = ?', "View Only").uniq
     end
 
+    # returns array of projects in which the (current) user is a Collaborator in any role
+    def self.collaborator_of_any_kind
+        joins(:collaborations).uniq
+        # project_user_is_primary_or_owner_or_update(user) + project_user_is_viewonly(user)
+    end
+
+    # returns array of projects in which the (current) user is a Primary Project Owner
+    def self.primary_owner 
+        joins(:collaborations).where('collaborations.role = ?', "Primary Project Owner").uniq
+    end
+
+    # returns array of projects in which the (current) user is Project Owner
+    def self.owner 
+        joins(:collaborations).where('collaborations.role = ?', "Project Owner").uniq
+    end
+
+    # returns array of projects in which the (current) user is Update and View
+    def self.update_and_view 
+        joins(:collaborations).where('collaborations.role = ?', "Update and View").uniq
+    end
+
+    # returns array of projects in which the (current) user is View Only
+    def self.view_only 
+        joins(:collaborations).where('collaborations.role = ?', "View Only").uniq
+    end
+
+    # returns array of projects in which the (current) user is Primary OR Project Owner
+    # would be better with an or query - need to figure out synta
+    def self.primary_or_owner 
+        primary_owner + owner
+    end
 
 
-        # def self.project_user_is_primary_or_owner_or_update(user)
-        #     project_user_is_primary_owner(user) + project_user_is_owner(user) + project_user_is_update(user).compact
-        # end
+
 
 end
