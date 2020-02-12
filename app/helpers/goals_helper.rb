79db7823651c0a2a8c8bc162e361a21d5a32dee0
jessesbyers <jesse.s.byers@gmail.com>
2020-02-12 13:13:43 -0500

    golas show page reformatted with partials and helpers

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,2 +1,14 @@
 module GoalsHelper
+
+    def add_(object)
+        if current_user.projects.primary_or_owner_or_update.include?(@goal.project)
+            link_to "Add an #{object.capitalize} for this Goal", "new_goal_#{object}_path(@goal)"
+        end
+    end
+
+    def view_all_(object)
+        if "@goal.#{object}s.any?"
+            link_to "View All #{object.capitalize}s for this Goal", "goal_#{object}s_path(@goal)"
+        end
+    end
 end
--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,18 +1,5 @@
 module ImagesHelper
 
-    def add_image
-        if current_user.projects.primary_or_owner_or_update.include?(@goal.project)
-            link_to "Add an Image for this Goal", new_goal_image_path(@goal)
-        end
-    end
-
-    def view_all_images
-        if @goal.images.any?
-            link_to "View All Images for this Goal", goal_images_path(@goal)
-        end
-    end
-
-
     def view_latest_image
         if @goal.images.any?
             image_tag(@goal.images.last.url)
