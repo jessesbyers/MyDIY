8f4cde8114a25cc38f3169ae2bb006f033682890
jessesbyers <jesse.s.byers@gmail.com>
2020-02-18 10:35:50 -0500

    trying new route for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
-  get '/auth/:provider/callback' => 'sessions#omniauth'
+  # get '/auth/:provider/callback' => 'sessions#omniauth'
+  get '/auth/google_oauth2/callback' => 'sessions#omniauth'
+
 
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
